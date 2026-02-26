a9ae69f08c58ba0391e60d61c3592150f7720c20
Arvid Gussarsson <arvid.gussarsson@gmail.com>
2026-02-26 23:15:06 +0100

    feat: Run-ready model evaluation file (generalization test).

--- src/gary/ml/evaluate_model.py
+++ src/gary/ml/evaluate_model.py
@@ -1,20 +1,17 @@
 import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import gary
-from pathlib import Path
-
-from gary.ml.model import MultiHeadClassifier
-from gary.ml.dataset import ExceptionsDataset, LabelMaps
-import gary
-from pathlib import Path
-
 from sklearn.metrics import f1_score, matthews_corrcoef
 
-import json
+import gary
+from gary.ml.model import MultiHeadClassifier
+from gary.ml.dataset import ExceptionsDataset, LabelMaps
 
+from pathlib import Path
+import json
 import os
+import argparse
+
 
 EVENT_TYPES = ["none", "too_heavy", "too_light", "pain", "time", "equipment", "form", "other"]
 SEVERITIES = ["na", "low", "medium", "high"]
@@ -26,12 +23,22 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Running on {device}.")
 
-    data_path = Path(gary.__file__).parent / "data" / "test.csv"
-    PROJECT_ROOT = Path(__file__).resolve().parents[3]
-    out_dir = PROJECT_ROOT / "artifacts"
-    model_dir = out_dir / "best_model.pt"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run", type=str, required=True)
+    args = parser.parse_args()
+
+    run_dir = Path(args.run)
+    model_dir = run_dir / "model.pt"
+    config_dir = run_dir / "config.json"
     
-    config = 
+    with open(config_dir, "r") as f:
+        config = json.load(f)
+
+
+    data_path = Path(gary.__file__).parent / "data" / "test.csv"
+    out_dir = run_dir / "test_metrics"
+    out_dir.mkdir(parents=True, exist_ok=True)
+    
 
     seed = int(os.getenv("SEED", "42"))
 
@@ -42,7 +49,7 @@ def main():
 
     test_ds = ExceptionsDataset(data_path, base_model, label_maps, max_len=256)
 
-    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=config["batch_size"], shuffle=True)
 
     model = MultiHeadClassifier(base_model, num_event_types=len(EVENT_TYPES), num_severities=len(SEVERITIES))
     model.to(device)
@@ -64,9 +71,8 @@ def main():
 
             out = model(**batch)
 
-            # adjust keys if needed
             event_logits = out["event_logits"]
-            sev_logits = out["sev_logits"]
+            sev_logits = out["severity_logits"]
 
             event_preds = torch.argmax(event_logits, dim=1)
             sev_preds = torch.argmax(sev_logits, dim=1)
